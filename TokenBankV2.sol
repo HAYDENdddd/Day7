ca773498fe89159adfc2ad5eaf487db12f952185
HAYDENdddd <161107566+HAYDENdddd@users.noreply.github.com>
2025-07-16 22:18:15 +0800

    Update TokenBankV2.sol

--- TokenBankV2.sol
+++ TokenBankV2.sol
@@ -8,19 +8,21 @@ interface IERC20 {
         uint256 amount
     ) external returns (bool);
 
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
 }
 
-contract TokenBank {
+contract TokenBankV2 {
     IERC20 public token; // ERC20 Token 合约引用
     event Received(address from, uint amount, string msg);
     // 每个地址的存款余额
-    mapping(address => uint256) public balances;
-    
+    mapping(address => uint256) public balances; // 用于 deposit/withdraw
+    mapping(address => uint256) public deposits; // 用于 tokensReceived
+
     constructor(address tokenAddress) {
-        token = IERC20(tokenAddress); // ✅ 使用传入参数
+        token = IERC20(tokenAddress); //使用传入参数
     }
 
     function deposit(uint256 amount) external {
@@ -47,6 +49,7 @@ contract TokenBank {
         uint256 amount,
         bytes calldata data
     ) external {
+        deposits[from] += amount;
         emit Received(from, amount, string(data));
         // 响应收到钱：比如登记、铸造NFT、处理订单等
     }
